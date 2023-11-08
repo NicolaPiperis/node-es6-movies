875628fd480815b967791006438fbd5d9052933a
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-11-08 17:41:55 +0100

    array di tutti i generi

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ Entrambe le classi dovranno avere un metodo toString() che ritorni una stringa c
 Jaws è un film di genere Drama. E’ stato rilasciato nel 1975 ed ha un voto di 8
 Breaking Bad è una serie tv di genere Drama. La prima stagione è stata rilasciato nel 2008 ed in totale sono state prodotte 5 stagioni. Ha un voto di 9.5
 Tramite la funzione .map(), creare un nuovo array dove per ogni elemento dell’array di oggetti viene creata un istanza della classe Movie o TvSerie in base al type e salvata nel nuovo array.
-
 Creiamo una funzione che restituisca la media dei voti di tutti i film per un determinato genere. Prevedere un argomento per la lista dei film ed uno per il genere.
+
 Creiamo una funzione che restituisca la lista di tutti i generi dei film, senza che questi si ripetano.
 Creiamo una funzione che filtri i film in base ad un genere passato come argomento e ne ritorni un array con all’interno il risultato della funzione toString() di ogni film.
 
@@ -141,6 +141,18 @@ function avarageRatingfunction(movies, genre) {
     return avarageRating;
 
 };
-
 console.log(avarageRatingfunction(movies, "Drama"));
+
+function genreArrayFunction(movies) {
+    const genreArray = [];
+    movies.forEach(movie => {
+        if (!genreArray.includes(movie.genre)) {
+            genreArray.push(movie.genre);
+        }
+    });
+    return genreArray;
+};
+
+
+console.log(genreArrayFunction(movies));
 
